33781886eb8a51bf12e0207d8fb35c2d6cc1ff26
Christopher Woodward <christopherwoodward@Christophers-iMac.local>
2018-03-20 00:13:13 -0700

    deployed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 
 import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   public currentRoute;
-  constructor(private router: Router) {
+  constructor(private router: Router, private meta: Meta) {
     this.router.events.subscribe((event) => {
         if (event instanceof NavigationEnd) {
           this.currentRoute = event.url;
         }
     });
+    this.meta.addTag({ name: 'description', content: 'MeditateRedding.com serves to centralize and consolidate as much information as possible related to meditation practice in the Shasta County area of Northern California. We would like for Shasta County residents or passers-through to be able to find local meditation gatherings, seek out nearby retreats and connect with other like-minded individuals.' });
+    this.meta.addTag({ name: 'author', content: 'Chris Woodward' });
+    this.meta.addTag({ name: 'keywords', content: 'Meditate Redding California Ca Buddhism Shasta Northern Spiritual Meditation Buddhism' });
   }
 
   ngOnInit() {}
